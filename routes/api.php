3d63fe71eb8ac7ba24709d50a633810abec4c8f1
Maksim <mobooss@gmail.com>
2021-10-23 12:55:23 +0300

    add models and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WorkerController;
+use App\Http\Controllers\DepartmentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('worker', WorkerController::class);
 
-
-
-
-
+Route::resource('department', DepartmentController::class);
 
